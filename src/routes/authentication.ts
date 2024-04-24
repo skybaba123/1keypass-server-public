5691eea0003f01228140d67cc36a17c5605ef27d
Ugochukwu Miracle <ugwumiracle123@gmail.com>
2024-04-24 03:08:20 +0100

    added subscription verification

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -11,6 +11,8 @@ import {
 } from "../controllers/authentication";
 import express, { Request, Response } from "express";
 import { UserTypes } from "../types/UserTypes";
+import User from "../models/user";
+import axios from "axios";
 
 interface AuthRequest extends Request {
   user: UserTypes;
@@ -18,14 +20,45 @@ interface AuthRequest extends Request {
 
 const router = express.Router();
 
-
-
 router.get(
   "/authenticate-user",
   userAuth,
   async (req: AuthRequest, res: Response) => {
     try {
-      return res.status(200).send(req.user);
+      const user = await User.findById(req.user._id);
+      if (!user) return res.status(404).send({ error: "No user found" });
+
+      const { data: customerInfo } = await axios.get(
+        `https://api.revenuecat.com/v1/subscribers/${user._id.toString()}`,
+        {
+          headers: { Authorization: `Bearer ${process.env.RC_ANDROID_PUBKEY}` },
+        }
+      );
+
+      console.log(customerInfo);
+      console.log(customerInfo.subscriber);
+      console.log(customerInfo.subscriber.entitlements);
+
+      const isSubscribed = customerInfo.subscriber.entitlements.active["pro"];
+
+      console.log(isSubscribed);
+
+      let updatedUser;
+      if (isSubscribed) {
+        updatedUser = await User.findByIdAndUpdate(
+          user._id,
+          { plan: "premium" },
+          { new: true }
+        );
+      } else {
+        updatedUser = await User.findByIdAndUpdate(
+          user._id,
+          { plan: "free" },
+          { new: true }
+        );
+      }
+
+      return res.status(200).send(updatedUser);
     } catch (error) {
       return res.status(500).send({ error: error.message });
     }
